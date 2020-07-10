1b97b4a6738551edf0d57bf712af24636d882344
Javier Díaz Gómez <javiercoree@gmail.com>
2020-07-09 22:29:55 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import { IconClose } from "./components/icons/Close";
+import { IconSearch } from "./components/icons/Search";
+import { IconClock } from "./components/icons/Clock";
 
 function App() {
+  const [value, setValue] = React.useState<string>("");
+
+  function onChange({ target }: React.ChangeEvent<HTMLInputElement>) {
+    setValue(target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Search-container">
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder="Search"
+          className={`Search-input ${value !== "" ? "active" : ""}`}
+        />
+
+        {value !== "" && (
+          <button
+            type="button"
+            className="Search-clear"
+            onClick={() => setValue("")}
+          >
+            <IconClose />
+          </button>
+        )}
+
+        <button type="button" className="Search-button" disabled={value === ""}>
+          <IconSearch />
+        </button>
+      </div>
+      {value !== "" && (
+        <div className="Search-result">
+          <button className="Search-result-item">
+            <span>
+              <IconSearch /> {value}
+            </span>
+          </button>
+          <button className="Search-result-item history">
+            <span>
+              <IconClock /> This is a text
+            </span>
+            <button className="Search-result-clear">
+              <IconClose />
+            </button>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/icons/Clock.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export function IconClock() {
+  return (
+    <svg
+      width="20px"
+      height="20px"
+      version="1.1"
+      viewBox="0 0 20 20"
+      x="0px"
+      y="0px"
+    >
+      <g>
+        <path d="M5.757 14.243A6 6 0 105.527 6H7v2H2V3h2v1.708a8 8 0 11.343 10.949l1.414-1.414z" />
+        <path d="M11 10.414l1.707-1.707-1.414-1.414L9 9.586V14h2v-3.586z" />
+      </g>
+    </svg>
+  );
+}
